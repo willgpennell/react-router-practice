b4754c8727a3710c843a66bbeb7f6289c663bc97
willgpennell <wpennell48@gmail.com>
2024-05-28 14:59:23 -0230

    links to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Contact from "./pages/Contact";
@@ -7,10 +7,17 @@ function App() {
   return (
     <div>
       <Router>
+        <div>
+          NAV BAR
+          <Link to={"/"}>Home</Link>
+          <Link to={"/Menu"}>Menu</Link>
+          <Link to={"/Contacts"}>Contact</Link>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Menu" element={<Menu />} />
           <Route path="/Contacts" element={<Contact />} />
+          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
         </Routes>
       </Router>
     </div>
