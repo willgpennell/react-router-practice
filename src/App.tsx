0ddfb322b48600f8faf798f50bcbe207b137cb82
willgpennell <wpennell48@gmail.com>
2024-05-28 14:51:58 -0230

    home menu contacts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Contacts" element={<Contact />} />
         </Routes>
       </Router>
     </div>
